85b43f62f27730f30e52cbac33fc1d696041b525
Jashan Randhawa <147901333+Jashan-randhawa@users.noreply.github.com>
2026-04-01 10:59:15 +0530

    Create calorie-estimate.ts

---
+++ server/src/api/calorie-estimate/routes/calorie-estimate.ts
@@ -0,0 +1,12 @@
+export default {
+  routes: [
+    {
+      method: "POST",
+      path: "/calorie-estimate",
+      handler: "calorie-estimate.estimate",
+      config: {
+        auth: { scope: ["authenticated"] },
+      },
+    },
+  ],
+};
