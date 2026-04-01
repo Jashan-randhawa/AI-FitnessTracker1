beed7986a221319fd5c8ee5a1dccbdc6968a94dc
Jashan Randhawa <147901333+Jashan-randhawa@users.noreply.github.com>
2026-04-01 10:56:37 +0530

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -23,6 +23,7 @@ async function setAuthenticatedPermissions(strapi: Core.Strapi) {
     'api::activitylog.activitylog.delete',
     'api::image-analysis.image-analysis.analyze',
     'api::ai-assistant.ai-assistant.chat',
+    'api::calorie-estimate.calorie-estimate.estimate',
     'api::blog.blog.find',
     'api::blog.blog.findOne',
   ];
