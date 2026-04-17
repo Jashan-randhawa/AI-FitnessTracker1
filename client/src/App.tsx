855423c5c3185635db036baa82e691b1366df791
Jashan Randhawa <147901333+Jashan-randhawa@users.noreply.github.com>
2026-04-17 18:31:44 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,6 +11,7 @@ import Weather from "./Pages/Weather";
 import Workouts from "./Pages/Workouts";
 import MealPlanner from "./Pages/MealPlanner";
 import ActivityPlanner from "./Pages/ActivityPlanner";
+import SpotifyPlaylist from "./Pages/SpotifyPlaylist";
 import { useappcontext } from "./Context/AppContext";
 import Login from "./Pages/Login";
 import Loading from "./components/Loading";
@@ -49,6 +50,7 @@ const App = () => {
                 <Route path="workouts"    element={<Workouts />} />
                 <Route path="planner"     element={<MealPlanner />} />
                 <Route path="activity-planner" element={<ActivityPlanner />} />
+                <Route path="spotify"          element={<SpotifyPlaylist />} />
               </Route>
             </Routes>
           )
